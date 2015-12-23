2de50393e6176e59450c880b2ffce21c16c25ea7
Vera <vvasko@spaceotechnologies.ru>
2015-12-23 19:56:18 +0200

    dummy main page and main menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'admin', to: redirect('admin/enterprise')
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root 'promos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
