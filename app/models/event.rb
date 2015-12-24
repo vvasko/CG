f1891af12f3e18625bac184411d632dd25b2a345
uncle.istvan55@gmail.com <uncle.istvan55@gmail.com>
2015-12-24 22:42:53 +0200

    added some validations and default image

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,21 @@ class Event < ActiveRecord::Base
   has_many :event_times
   belongs_to :enterprise
   validates :name, :description, :start_date, :final_date, presence: true, allow_blank: false
-
-
   scope :date_filter, ->(filter_date) { where("start_date <= ?", filter_date).where("final_date >= ?", filter_date)}
+
+  validate :final_date_must_be_after_start_date
+  validate :date_must_be_in_future, on: :create
+
+  def final_date_must_be_after_start_date
+    if start_date > final_date
+      errors.add(:final_date, 'should be AFTER start date')
+    end
+  end
+
+  def date_must_be_in_future
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 
 end
