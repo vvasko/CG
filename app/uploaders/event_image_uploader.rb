f55a33cbf929cdd34c4db87c0822561cb4d62891
uncle.istvan55@gmail.com <uncle.istvan55@gmail.com>
2015-12-24 22:56:23 +0200

    added another default image

--- app/uploaders/event_image_uploader.rb
+++ app/uploaders/event_image_uploader.rb
@@ -21,7 +21,7 @@ class EventImageUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-     "images/fallback/" + [version_name, "default_event.png"].compact.join('_')
+     "fallback/" + [version_name, "default_event.png"].compact.join('_')
    end
 
   # Process files as they are uploaded:
