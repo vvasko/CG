01be47367a8ca596f022022596d24cdd45e0295b
Vera <vvasko@spaceotechnologies.ru>
2015-12-25 00:49:52 +0200

    fix styles and enterprise description field

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223171556) do
+ActiveRecord::Schema.define(version: 20151224180138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "enterprises", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,9 +29,9 @@ ActiveRecord::Schema.define(version: 20151223171556) do
     t.string   "phone"
     t.string   "email"
     t.string   "picture"
-    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "event_times", force: :cascade do |t|
