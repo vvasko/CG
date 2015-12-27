c1a769964b7f53c181dd505ff3054b57686cc0d8
Sergei Belokon <sergei.e.belokon@gmail.com>
2015-12-27 19:26:10 +0200

    Minor changes

--- config/navigation.rb
+++ config/navigation.rb
@@ -7,11 +7,11 @@ SimpleNavigation::Configuration.run do |navigation|
     navigation.selected_class = 'active'
     navigation.items do |primary|
       primary.dom_class = 'nav navbar-nav'
-      primary.item :menu_home, 'Home', root_path
-      primary.item :menu_enterprises, 'Enterprises', enterprises_path, highlights_on: :subpath
+      # primary.item :menu_home, 'Home', root_path
       primary.item :menu_promos, 'Promos', promos_path,  highlights_on: :subpath
       primary.item :menu_events, 'Events', events_path,  highlights_on: :subpath
       primary.item :menu_vacancies, 'Vacancies', vacancies_path,  highlights_on: :subpath
+      primary.item :menu_enterprises, 'Enterprises', enterprises_path, highlights_on: :subpath
       primary.item :menu_login, 'Enterprise Admin',  new_user_session_path, unless: proc { user_signed_in? }
       primary.item :menu_logout, 'Enterprise Admin', admin_path,  if: proc { user_signed_in? }
 
